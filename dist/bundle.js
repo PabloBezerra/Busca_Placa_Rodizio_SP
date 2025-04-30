32f59edf97e12968b9cfc92d0e51aa0559d4b3d9
Pablo da Silva Bezerra <Pdsbezerra1316@gmail.com>
2025-04-29 22:44:52 -0300

    Primeiro Commit

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+console.log("olá mundo")
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry : "./src/app.js",
+    output :{ 
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devtool: "source-map",
+    module :{
+        rule :[
+            {
+
+            }
+        ]
+    }
+}
